6d3295d3dd00841bebbc48efc3873de2645f80b8
Carlos Pérez Armenta <27734762+cparmenta@users.noreply.github.com>
2022-12-04 11:03:36 +0100

    Day 4 solved
    Also made day 3 part 2 a bit more efficient.

--- 03/solve_2.py
+++ 03/solve_2.py
@@ -1,14 +1,12 @@
-def get_item(priority):
-	if 1 <= priority <= 26:
-		item = chr(priority + ord('a') - 1)
-	elif 27 <= priority <= 52:
-		item = chr(priority - 27 + ord('A'))
-	else:
-		item = None
-	return item
+def get_priority(item):
+    item_lowercase = item.lower()
+    priority = ord(item_lowercase) - ord('a') + 1
+    if ord('A') <= ord(item) <= ord('Z'):
+        priority += 26
+    return priority
 
 def read_group(file):
-	return [file.readline() for i in range(3)]
+	return [file.readline()[:-1] for i in range(3)] # :-1 excludes \n
 
 file = open('input')
 
@@ -16,10 +14,9 @@ sum_priorities = 0
 
 group = read_group(file)
 while group[0] != '':
-	for p in range(1, 53):
-		item = get_item(p)
-		if item in group[0] and item in group[1] and item in group[2]:
-		   sum_priorities += p
+	for item in set(group[0]):
+		if item in group[1] and item in group[2]:
+		   sum_priorities += get_priority(item)
 	group = read_group(file)
 
 print(f'{sum_priorities = }')
