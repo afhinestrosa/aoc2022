ea3de54c4d17c072605fcc7ce0e6185a1d4b8895
Carlos Pérez Armenta <27734762+cparmenta@users.noreply.github.com>
2022-12-07 18:53:31 +0100

    Day 7 solved

---
+++ 07/solve.py
@@ -0,0 +1,84 @@
+SIZE_MAX = 100000
+
+class Node:
+	solution = 0
+
+	def __init__(self, name, type, size, parent=None):
+		self.name = name
+		self.type = type
+		self.size = size
+		self.children = []
+		self.parent = parent
+
+	def add_child(self, node):
+		self.children.append(node)
+		node.parent = self
+
+	def __str__(self):
+		if self.type == 'file':
+			text = f'{self.name} ({self.type}, size={self.size})\n'
+		else:
+			extra_text = f', size={self.size}' if self.size > 0 else ''
+			text = f'{self.name} (dir{extra_text})\n'
+			for child in self.children:
+				text += str(child)
+		return text
+
+	def calculate_size(self):
+		if self.type == 'dir':
+			size = 0
+			for child in self.children:
+				size += child.calculate_size()
+			if size <= SIZE_MAX:
+				Node.solution += size
+			self.size = size
+		return self.size
+
+
+
+class Tree(Node):
+	def __init__(self):
+		super().__init__('/', 'dir', 0)
+
+
+
+status = 'WAIT_CMD'
+root = Tree()
+current_dir = root
+
+with open('input') as file:
+	line = file.readline() # skip cd /
+
+	line = file.readline().split()
+	while len(line) > 0:
+		print(f'Processing: {line}')
+		if status == 'WAIT_CMD':  # line[0]: $; line[1]: cmd; line[2]: arg;
+			if line[1] == 'ls':
+				status = 'LIST_DIR'
+			elif line[1] == 'cd':
+				if line[2] == '..':
+					current_dir = current_dir.parent
+				else:
+					current_dir = next(x for x in current_dir.children if x.name == line[2])
+		elif status == 'LIST_DIR':
+			if line[0] == '$':
+				status = 'WAIT_CMD'
+				continue
+			elif line[0] == 'dir':
+				current_dir.add_child(Node(
+					name=line[1],
+					type='dir',
+					size=-1,
+				))
+			else:
+				current_dir.add_child(Node(
+					name=line[1],
+					type='file',
+					size=int(line[0]),
+				))
+
+		line = file.readline().split()
+
+root.calculate_size()
+print(root)
+print(f'{Node.solution = }')
